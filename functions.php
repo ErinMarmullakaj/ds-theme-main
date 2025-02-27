8de87db5d9dcd60afcece57de011bf1f6a755c93
ErinMarmullakaj <erin.marmullakaj2@gmail.com>
2025-02-27 17:41:44 +0100

    day6

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function add_theme_scripts(){
-    wp_enqueue_style('style', get_template_directory_uri()."/style.css", false, '1.0', "all");    
+    wp_enqueue_style('style', get_template_directory_uri()."/style.css", false, '1.0', "all");
+    wp_enqueue_style('main', get_template_directory_uri()."css/main.css", false, '1.0', "all");    
     wp_enqueue_scripts("main", get_template_directory_uri()."/js/main.js", array('jquary'), "1.0", true);
 
     if(is_singular() && comments_open() && get_option('thread-comments')){
--- page-about-me.php
+++ page-about-me.php
@@ -5,16 +5,27 @@
 
 <?php get_header(); ?>
 
+<div class="container">
+    <div class="first">
+        <h1>about page posts </h1>
+        <?php
+            if(have_posts()):
+            while(have_posts()):the_post();?>
+            <h2><?php the_title(); ?> </h2>
+            <p><?php  the_content(); ?> </p>
+            <?php endwhile; ?>
+            <?php endif;  ?>
+
+    </div>
+    <div class="second">
+        <h1>sidebar</h1>
+    </div>
+
+</div>
 
 
-<h1>hello world </h1>
-<?php
-    if(have_posts()):
-        while(have_posts()):the_post();?>
-        <h2><?php the_title(); ?> </h2>
-        <p><?php  the_content(); ?> </p>
-    <?php endwhile; ?>
-    <?php endif;  ?>
+
+>
 
 
 <?php get_footer(); ?>
